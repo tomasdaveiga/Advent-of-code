4e3446b030a277197291b861a206ece910ddc9af
tomasdaveiga <tomasdaveiga@gmail.com>
2023-12-26 18:52:14 +0000

    day 2 of 2016 done

--- 2016/app/Main.hs
+++ 2016/app/Main.hs
@@ -1,8 +1,13 @@
 module Main (main) where
 import DayOne
+import DayTwo
 
 main :: IO ()
 main = do
-    contents <- readFile "input/day01.txt"
-    print $ dayOnePart1 contents
-    print $ dayOnePart2 contents
+    contentsDay1 <- readFile "input/day01.txt"
+    print $ dayOnePart1 contentsDay1
+    print $ dayOnePart2 contentsDay1
+
+    contentsDay2 <- readFile "input/day02.txt"
+    print $ dayTwoPart1 contentsDay2
+    print $ dayTwoPart2 contentsDay2
